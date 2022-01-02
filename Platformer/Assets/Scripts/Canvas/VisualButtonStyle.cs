f0e524916df15612ec3b868797488f630f795075
IIIEA <egor.solo2000@bk.ru>
2022-01-02 13:23:01 +0300

    OpMenu

--- Platformer/Assets/Scripts/Canvas/VisualButtonStyle.cs
+++ Platformer/Assets/Scripts/Canvas/VisualButtonStyle.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VisualButtonStyle : MonoBehaviour
 {
+    [SerializeField] private RectTransform _rectTransform;
+
+    private Vector3 _startTransform;
+    private Vector3 _targetTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _startTransform = _rectTransform.localScale;
+    }
+
+    private void OnMouseEnter()
+    {
+
+    }
 
 }
